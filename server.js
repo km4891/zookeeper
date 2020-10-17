1c16bcf198697ee0cd588c9edf1e8dfa80346cae
km4891 <kmorarity@gmail.com>
2020-10-16 21:53:30 -0700

    3 fin

--- server.js
+++ server.js
@@ -2,12 +2,6 @@ const express = require('express');
 const { animals } = require('./data/animals');
 const fs = require('fs');
 const path = require('path');
-
-
-
-
-
-
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -15,6 +9,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -118,6 +113,18 @@ function filterByQuery(query, animalsArray) {
             res.json(animal);
         }
         });
+
+        app.get('/', (req, res) => {
+            res.sendFile(path.join(__dirname, './public/index.html'));
+          });
+
+        app.get('/animals', (req, res) => {
+            res.sendFile(path.join(__dirname, './public/animals.html'));
+        });
+
+        app.get('/zookeepers', (req, res) => {
+            res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+          });
 
       app.listen(PORT, () => {
         console.log(`API server now on port ${PORT}!`);
