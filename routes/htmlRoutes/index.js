79a4b33fd33016adc9c949203a99eb57000cead1
km4891 <kmorarity@gmail.com>
2020-10-17 22:38:49 -0700

    finished

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,16 +1,21 @@
-const path = require("path");
-const router = require("express").Router();
+const path = require('path');
+const router = require('express').Router();
 
-router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../public/index.html"));
+//Transfers the file at the given path. Sets the Content-Type response HTTP header field based on the filename’s extension.
+
+//route to the index.html
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
-router.get("/animals", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../public/animals.html"));
+//route to the animals.html
+router.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/animals.html'));
 });
 
-router.get("/zookeepers", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../public/zookeepers.html"));
+//route to the zoop keepers
+router.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/zookeepers.html'));
 });
 
 module.exports = router;
